4886836a72f79a62f6c72885d098355b79df76b6
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-07-22 21:39:58 -0500

    Made a GameManger Script to handle the player's Health

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set;}
+
+    public Health playerHealth;
+    
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+            return;
+        }
+        playerHealth = GetComponent<Health>();
+    }
+
+    
+}
