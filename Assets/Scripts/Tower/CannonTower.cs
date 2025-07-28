c00687b2cdfac2a013106a0df815040562986a46
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-07-28 12:41:05 -0500

    Removed Rigidbody from CannonBall Prefab and GetComponent.Rigidbody line of code.

--- Assets/Scripts/Tower/CannonTower.cs
+++ Assets/Scripts/Tower/CannonTower.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class CannonTower : Tower
 {
-    public float LaunchVelocityY = 750f;
 
     [SerializeField] private GameObject projectilePrefab;
     
@@ -17,7 +16,6 @@ public class CannonTower : Tower
         if (projectilePrefab != null)
         {
             GameObject projectileInstance = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-            projectileInstance.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, LaunchVelocityY, 0));
             projectileInstance.GetComponent<Projectile>().SetTarget(target.transform);
         }
     }
