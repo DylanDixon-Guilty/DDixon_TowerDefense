5e7367d8c0b19d0838804b12601288b2c1ebea3a
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-07-30 17:05:52 -0500

    Made a function called Explosion that when it enters the Enemy's collider, it will find any other colliders within a 2f radius. then it will destroy any enemies within said radius. Also has a particle effect to visually show the explosion.

--- Assets/Scripts/Projectile/CannonProjectile.cs
+++ Assets/Scripts/Projectile/CannonProjectile.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class CannonProjectile : Projectile
 {
+    public float BlastRadius = 2f;
+    public GameObject ExplosionEffect;
+
     private Rigidbody cannonRb;
 
     private void Awake()
@@ -15,8 +18,35 @@ public class CannonProjectile : Projectile
         base.Update();
     }
 
+    /// <summary>
+    /// Set to find the Transform of the Enemy's position
+    /// </summary>
     public override void SetTarget(Transform inputTarget)
     {
         target = inputTarget;
+    }
+
+    private void OnTriggerEnter()
+    {
+        Explode();
+    }
+
+    /// <summary>
+    /// When the cannon ball hits an Enemy, it will find any colliders within a 2f(BlastRadius) radius.
+    /// Then Destroy all Enemies in that radius
+    /// </summary>
+    protected void Explode()
+    {
+        Instantiate(ExplosionEffect, transform.position, transform.rotation);
+        Collider[] enemyColliders = Physics.OverlapSphere(transform.position, BlastRadius);
+        foreach (Collider nearbyEnemies in enemyColliders)
+        {
+            Enemy enemy = nearbyEnemies.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                Destroy(enemy.gameObject);
+            }
+        }
+        Destroy(gameObject);
     }
 }
