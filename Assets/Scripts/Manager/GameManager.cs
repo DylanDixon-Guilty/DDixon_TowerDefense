c4bc5c0f6f329279fc3265a6ae25c5de74c96721
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-08-01 21:14:29 -0500

    Added an Update and BackToGame functions that handles the player being able to pause the game and mess with the Audio settings. Later will add a Function to go back to the Main Menu.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,12 +1,14 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set;}
+    public static bool IsGamePaused = false;
 
     public Health playerHealth;
-    
+    public GameObject OptionsMenuScreenInGame;
+    public GameObject MainHUDScreen;
+
     void Start()
     {
         if (instance == null)
@@ -19,5 +21,37 @@ public class GameManager : MonoBehaviour
             return;
         }
         playerHealth = GetComponent<Health>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !IsGamePaused)
+        {
+            IsGamePaused = true;
+            Time.timeScale = 0f;
+            MainHUDScreen.SetActive(false);
+            OptionsMenuScreenInGame.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && IsGamePaused)
+        {
+            IsGamePaused = false;
+            Time.timeScale = 1f;
+            MainHUDScreen.SetActive(true);
+            OptionsMenuScreenInGame.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// On Pressing "Back" button while on the Options Screen on any level, go back to the game. 
+    /// </summary>
+    public void BackToGame()
+    {
+        if (IsGamePaused)
+        {
+            IsGamePaused = false;
+            Time.timeScale = 1f;
+            MainHUDScreen.SetActive(true);
+            OptionsMenuScreenInGame.SetActive(false);
+        }
     }
 }
