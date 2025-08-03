53dc79ba14529d8aa9e94020f6c837d6dd56e3be
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-08-03 13:19:58 -0500

    Made functions that handles Saving the volume of Master, Music, and Sound effects between scenes or if the player quits and re-enters the game.

--- Assets/Scripts/MenuScripts/SoundSliderMenu.cs
+++ Assets/Scripts/MenuScripts/SoundSliderMenu.cs
@@ -1,7 +1,7 @@
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 public class SoundSliderMenu : MonoBehaviour
 {
@@ -9,34 +9,47 @@ public class SoundSliderMenu : MonoBehaviour
     public AudioClip soundEffectClip;
 
     [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private Slider master;
-    [SerializeField] private Slider soundEffect;
-    [SerializeField] private Slider music;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider soundEffectSlider;
+    [SerializeField] private Slider musicSlider;
 
     private string masterVolume = "MasterVolume";
     private string musicVolume = "MusicVolume";
     private string soundEffectVolume = "SoundEffectVolume";
+    private const string masterVolumeKey = "MasterVolumeKey"; //Key-word to save PlayerPrefs
+    private const string musicVolumeKey = "MusicVolumeKey";
+    private const string soundEffectVolumeKey = "SoundEffectVolumeKey";
     private float volumeMultiplier = 20f; // This goes in the ChangeMaster/Music/Sound Functions
+    private int defaultVolume = 1; //The default of the volume if the player has not set it yet
 
-    private void Awake()
+    private void Start()
     {
-        if(PlayerPrefs.HasKey("masterVolume"))
-        {
-            LoadMasterVolume();
-        }
-        else
-        {
-            ChangeMaster();
-        }
+        LoadVolume(masterVolumeKey, masterSlider, masterVolume);
+        LoadVolume(musicVolumeKey, musicSlider, musicVolume);
+        LoadVolume(soundEffectVolumeKey, soundEffectSlider, soundEffectVolume);
+    }
 
-        if(PlayerPrefs.HasKey("musicVolume"))
-        {
-            LoadMusicVolume();
-        }
-        else
-        {
-            ChangeMusic();
-        }
+    /// <summary>
+    /// This changes the volume whenever the slider moved to the left or right
+    /// based on the parameters in the Functions(ChangeMaster/Music/SoundEffect) down below.
+    /// Then it saves it to a keyword given in said Functions
+    /// </summary>
+    private void ChangeVolume(string playerPrefsKey, string mixerParameter, float volume)
+    {
+        audioMixer.SetFloat(mixerParameter, Mathf.Log10(volume) * volumeMultiplier);
+        PlayerPrefs.SetFloat(playerPrefsKey, volume);
+        PlayerPrefs.Save();
+    }
+
+    /// <summary>
+    /// When the player changes any of the sliders they are saved in ChangeVolume.
+    /// Then this functions handles setting the values the player set when the player quits and re-enters the game based on that saved data
+    /// </summary>
+    private void LoadVolume(string playerPrefsKey, Slider slider, string mixerParameter)
+    {
+        float savedVolume = PlayerPrefs.GetFloat(playerPrefsKey, defaultVolume);
+        slider.value = savedVolume;
+        audioMixer.SetFloat(mixerParameter, Mathf.Log10(savedVolume) * volumeMultiplier);
     }
 
     /// <summary>
@@ -44,15 +57,7 @@ public class SoundSliderMenu : MonoBehaviour
     /// </summary>
     public void ChangeMaster()
     {
-        float volume = master.value;
-        audioMixer.SetFloat(masterVolume, Mathf.Log10(volume) * volumeMultiplier);
-        PlayerPrefs.SetFloat("masterVolume", volume);
-    }
-
-    private void LoadMasterVolume()
-    {
-        master.value = PlayerPrefs.GetFloat("masterVolume");
-        ChangeMaster();
+        ChangeVolume(masterVolumeKey, masterVolume, masterSlider.value);
     }
 
     /// <summary>
@@ -60,15 +65,7 @@ public class SoundSliderMenu : MonoBehaviour
     /// </summary>
     public void ChangeMusic()
     {
-        float volume = music.value;
-        audioMixer.SetFloat(musicVolume, Mathf.Log10(volume) * volumeMultiplier);
-        PlayerPrefs.SetFloat("musicVolume", volume);
-    }
-
-    private void LoadMusicVolume()
-    {
-        music.value = PlayerPrefs.GetFloat("musicVolume");
-        ChangeMusic();
+        ChangeVolume(musicVolumeKey, musicVolume, musicSlider.value);
     }
 
     /// <summary>
@@ -76,8 +73,7 @@ public class SoundSliderMenu : MonoBehaviour
     /// </summary>
     public void ChangeSoundEffect()
     {
-        float volume = master.value;
-        audioMixer.SetFloat(soundEffectVolume, Mathf.Log10(volume) * volumeMultiplier);
+        ChangeVolume(soundEffectVolumeKey, soundEffectVolume, soundEffectSlider.value);
     }
 
     /// <summary>
