9c699a7cf7a3b5d74497fd361f91c6b72d0b8673
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-08-07 18:45:43 -0500

    Add more Else If statements that will check which level the player is in and give a starting amount of Currency. Will later add actual numbers to currency.

--- Assets/Scripts/Manager/CurrencyManager.cs
+++ Assets/Scripts/Manager/CurrencyManager.cs
@@ -6,6 +6,9 @@ public class CurrencyManager : MonoBehaviour
 {
     public static int Currency;
     public string Level01Scene;
+    public string Level02Scene;
+    public string Level03Scene;
+    public string Level04Scene;
 
     private TextMeshProUGUI currencyScoreText;
 
@@ -18,6 +21,19 @@ public class CurrencyManager : MonoBehaviour
         {
             Currency = 200;
         }
+        else if(currentScene.name == Level02Scene)
+        {
+            Currency = 0;
+        }
+        else if(currentScene.name == Level03Scene)
+        {
+            Currency = 0;
+        }
+        else if(currentScene.name == Level04Scene)
+        {
+            Currency = 0;
+        }
+
     }
 
     
