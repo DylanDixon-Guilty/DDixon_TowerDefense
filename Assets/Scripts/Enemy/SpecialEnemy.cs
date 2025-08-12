d2d14e99ed57cb9364f8ef5dd2fb96cf91409492
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-08-11 19:34:00 -0500

    Fixed the JammerEnemy where it would not restore the Material of the Tower it disabled.

--- Assets/Scripts/Enemy/SpecialEnemy.cs
+++ Assets/Scripts/Enemy/SpecialEnemy.cs
@@ -9,62 +9,64 @@ public class SpecialEnemy : MonoBehaviour
     [SerializeField] private float effectLifeTime;
     [SerializeField] private int disableTowerBlastRadius;
     [SerializeField] private int enableTowerBlastRadius;
-    private int healthLimitForAbility = 10;
     private Dictionary<Tower, Material> originalMaterials = new Dictionary<Tower, Material>();
 
-    private void Update()
+    private void Start()
     {
-        DisableNearbyTowers();
+        Enemy enemy = GetComponent<Enemy>();
+        enemy.OnJammerBeingHit += DisableNearbyTowers;
     }
 
     /// <summary>
     /// When the JammerEnemy gets hit, it will disable a Tower within a 1f Radius and turn them blue.
-    /// The Freeze Tower intentionally does not proc this effect
+    /// The Freeze Tower intentionally does not proc this effect.
+    /// If the JammerEnemy has less than 20 Health, do not activate this Ability
     /// </summary>
     private void DisableNearbyTowers()
     {
-        Enemy enemy = GetComponent<Enemy>();
-        if(enemy.HasTakenDamage && enemy.CurrentHealth > healthLimitForAbility)
-        {
-            Collider[] towerColliders = Physics.OverlapSphere(transform.position, disableTowerBlastRadius);
-            foreach(Collider nearbyTowers in towerColliders)
-            {
-                Tower tower = nearbyTowers.GetComponent<Tower>();
-                if(tower != null)
-                {
-                    Renderer towerRenderer = tower.GetComponentInChildren<Renderer>();
-                    if(!originalMaterials.ContainsKey(tower))
-                    {
-                        originalMaterials[tower] = towerRenderer.material;
-                    }
-                    tower.GetComponentInChildren<Renderer>().material = Blue;
-                    tower.IsTowerPlaced = false;
-                }
-            }
-            enemy.HasTakenDamage = false;
-            StartCoroutine(DisableTowerTimer());
-        }
-    }
-
-    /// <summary>
-    /// The time that a Tower stays disable, after that time has elapsed, Renable the Tower.
-    /// Also, turn it back to its original material
-    /// </summary>
-    IEnumerator DisableTowerTimer()
-    {
-        yield return new WaitForSeconds(effectLifeTime);
-        Collider[] towerColliders = Physics.OverlapSphere(transform.position, enableTowerBlastRadius);
+        Collider[] towerColliders = Physics.OverlapSphere(transform.position, disableTowerBlastRadius);
         foreach(Collider nearbyTowers in towerColliders)
         {
             Tower tower = nearbyTowers.GetComponent<Tower>();
-            if (tower != null && !tower.IsTowerPlaced)
+            if (tower != null && tower.IsTowerPlaced != false)
             {
-                tower.IsTowerPlaced = true;
-                if(originalMaterials.TryGetValue(tower, out Material originalMaterial))
+                Renderer towerRenderer = tower.GetComponentInChildren<Renderer>();
+                if(!originalMaterials.ContainsKey(tower))
                 {
-                    tower.GetComponentInChildren<Renderer>().material = originalMaterial;
+                    originalMaterials[tower] = towerRenderer.material;
                 }
-                
+                tower.GetComponentInChildren<Renderer>().material = Blue;
+                tower.IsTowerPlaced = false;
+            }
+        }
+        StartCoroutine(DisableTowerTimer());
+        
+    }
+
+    /// <summary>
+    /// The time that a Tower stays disable, after that time has elapsed, reenable the Tower(s).
+    /// Also, turn it back to its original material
+    /// </summary>
+    private IEnumerator DisableTowerTimer()
+    {
+        yield return new WaitForSeconds(effectLifeTime);
+        EnableNearbyTowers();
+    }
+
+    /// <summary>
+    /// Reenable the Towers the Jammer Enemy disabled and give its original Material back.
+    /// Also used in the HasDied function in the Enemy script
+    /// </summary>
+    public void EnableNearbyTowers()
+    {
+        foreach(KeyValuePair<Tower, Material> originalTowerMaterial in originalMaterials)
+        {
+            Tower tower = originalTowerMaterial.Key;
+            Material towerMaterial = originalTowerMaterial.Value;
+            if(tower != null)
+            {
+                tower.GetComponentInChildren<Renderer>().material = towerMaterial;
+                tower.IsTowerPlaced = true;
             }
         }
         originalMaterials.Clear();
